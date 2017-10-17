14c1241bf8be99fe77aae7ec66ab4e129ea47d80
Alex Martin <gradient.am@gmail.com>
2017-10-16 18:24:06 -0700

    fixed out of order parameters

--- test_password.php
+++ test_password.php
@@ -6,12 +6,12 @@ require "password.php";
 $db = dbconn();
 $stmt = $db->stmt_init();
 $stmt->prepare("UPDATE TeachingAssistants SET hash = ? WHERE id = ?");
-$stmt->bind_param("ss", $_GET["username"], $_GET["password"]);
+$stmt->bind_param("ss", $_GET["password"], $_GET["username"]);
 
 if (!$stmt->execute()) {
     echo "ERROR: failed to execute query!";
 } else {
-    echo "SUCCESS!"
+    echo "SUCCESS!";
 }
 
 ?>
