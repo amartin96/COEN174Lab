ce27cd30e952308c0fa990b35214d17452e9a638
Alex Martin <gradient.am@gmail.com>
2017-10-16 18:27:21 -0700

    add missing function argument

--- test_password.php
+++ test_password.php
@@ -6,7 +6,7 @@ require "password.php";
 $db = dbconn();
 $stmt = $db->stmt_init();
 $stmt->prepare("UPDATE TeachingAssistants SET hash = ? WHERE id = ?");
-$stmt->bind_param("ss", password_hash($_GET["password"]), $_GET["username"]);
+$stmt->bind_param("ss", password_hash($_GET["password"], PASSWORD_DEFAULT), $_GET["username"]);
 
 if (!$stmt->execute()) {
     echo "ERROR: failed to execute query!";
