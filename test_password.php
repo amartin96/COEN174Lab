bf9f5737c7fd33e3ed488419fd50e999cbeee278
Alex Martin <gradient.am@gmail.com>
2017-10-16 18:17:26 -0700

    trying something

--- test_password.php
+++ test_password.php
@@ -3,15 +3,10 @@
 require "dbconn.php";
 require "password.php";
 
-if ($argc != 3) {
-    echo "Usage: php test_password.php USERNAME PASSWORD\n";
-    exit(1);
-}
-
 $db = dbconn();
 $stmt = $db->stmt_init();
 $stmt->prepare("UPDATE TeachingAssistants SET hash = ? WHERE id = ?");
-$stmt->bind_param("ss", $argv[1], $argv[2]);
+$stmt->bind_param("ss", $_GET["username"], $_GET["password"]);
 
 if (!$stmt->execute()) {
     echo "ERROR: failed to execute query!";
