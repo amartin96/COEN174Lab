03b5ab2b9ab07763a67dd60a76043b90b62338b0
Alex Martin <gradient.am@gmail.com>
2017-10-31 14:26:12 -0700

    test stuff

--- test_password_admin.php
+++ test_password_admin.php
@@ -4,7 +4,7 @@ require "helper.php";
 
 $db = connectToDatabase();
 $stmt = $db->stmt_init();
-$stmt->prepare("UPDATE TeachingAssistants SET hash = ? WHERE id = ?");
+$stmt->prepare("UPDATE Admins SET hash = ? WHERE username = ?");
 $stmt->bind_param("ss", password_hash($_GET["password"], PASSWORD_DEFAULT), $_GET["username"]);
 
 if (!$stmt->execute()) {
