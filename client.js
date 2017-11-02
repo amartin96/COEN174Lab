2c34187cf6766fe1d9f8536d668a2b30736d4e75
nicholas schnabel <nschnabe@linux60803.dc.engr.scu.edu>
2017-11-02 14:37:58 -0700

    add dynamic table for TA availability input

--- client.js
+++ client.js
@@ -10,7 +10,7 @@ $("#test-checkstatus").click(checkstatus);
 $("#test-submit").click(querySearch);
 $("#test-modify-submit").click(queryModifyInfo);
 $("#add-course-submit").click(queryAddCourse);
-$("#test-add-time-submit").click(queryAddTime);
+$("#test-save-time").click(queryAddTime);
 $("#test-clear-data").click(queryClearData);
 $("#test-password-submit").click(queryChangePassword);
 $("#test-admin-list").click(queryListUsers);
@@ -77,62 +77,60 @@ function querySearch()
     var day = $("#test-day").val();
     var t_start = $("#test-t_start").val();
     var t_end = $("#test-t_end").val();
-    t_start = ConvertTimeformat(t_start);
-    t_end = ConvertTimeformat(t_end);
     $.post("server.php", { query: "search", course: course, day: day, t_start: t_start, t_end: t_end }, function(data) {
         alert(data);
     });
 }
 
-$("#add-row-m").click(function(){
-
-    var counter = $('#m-table tr').length;
-
-    var markup = '<tr><td><input type="text" size = "8" id="M' + counter + '-start-time"></td><td> to </td><td><input type="text" size = "8" id="M' + counter + '-end-time"></td></tr>';
-
-    $('#m-table').append(markup);
-
-});
-
-$("#add-row-t").click(function(){
-
-    var counter = $('#t-table tr').length;
-
-    var markup = '<tr><td><input type="text" size = "8" id="T' + counter + '-start-time"></td><td> to </td><td><input type="text" size = "8" id="T' + counter + '-end-time"></td></tr>';
-
-    $('#t-table').append(markup);
-
-});
-
-$("#add-row-w").click(function(){
-
-    var counter = $('#w-table tr').length;
-
-    var markup = '<tr><td><input type="text" size = "8" id="W' + counter + '-start-time"></td><td> to </td><td><input type="text" size = "8" id="W' + counter + '-end-time"></td></tr>';
-
-    $('#w-table').append(markup);
-
-});
-
-$("#add-row-r").click(function(){
-
-    var counter = $('#r-table tr').length;
-
-    var markup = '<tr><td><input type="text" size = "8" id="R' + counter + '-start-time"></td><td> to </td><td><input type="text" size = "8" id="R' + counter + '-end-time"></td></tr>';
-
-    $('#r-table').append(markup);
-
-});
-
-$("#add-row-f").click(function(){
-
-    var counter = $('#f-table tr').length;
-
-    var markup = '<tr><td><input type="text" size = "8" id="F' + counter + '-start-time"></td><td> to </td><td><input type="text" size = "8" id="F' + counter + '-end-time"></td></tr>';
-
-    $('#f-table').append(markup);
-
-});
+//$("#add-row-m").click(function(){
+//
+//    var counter = $('#m-table tr').length;
+//
+//    var markup = '<tr><td><input type="text" size = "8" id="M' + counter + '-start-time"></td><td> to </td><td><input type="text" size = "8" id="M' + counter + '-end-time"></td></tr>';
+//
+//    $('#m-table').append(markup);
+//
+//});
+//
+//$("#add-row-t").click(function(){
+//
+//    var counter = $('#t-table tr').length;
+//
+//    var markup = '<tr><td><input type="text" size = "8" id="T' + counter + '-start-time"></td><td> to </td><td><input type="text" size = "8" id="T' + counter + '-end-time"></td></tr>';
+//
+//    $('#t-table').append(markup);
+//
+//});
+//
+//$("#add-row-w").click(function(){
+//
+//    var counter = $('#w-table tr').length;
+//
+//    var markup = '<tr><td><input type="text" size = "8" id="W' + counter + '-start-time"></td><td> to </td><td><input type="text" size = "8" id="W' + counter + '-end-time"></td></tr>';
+//
+//    $('#w-table').append(markup);
+//
+//});
+//
+//$("#add-row-r").click(function(){
+//
+//    var counter = $('#r-table tr').length;
+//
+//    var markup = '<tr><td><input type="text" size = "8" id="R' + counter + '-start-time"></td><td> to </td><td><input type="text" size = "8" id="R' + counter + '-end-time"></td></tr>';
+//
+//    $('#r-table').append(markup);
+//
+//});
+//
+//$("#add-row-f").click(function(){
+//
+//    var counter = $('#f-table tr').length;
+//
+//    var markup = '<tr><td><input type="text" size = "8" id="F' + counter + '-start-time"></td><td> to </td><td><input type="text" size = "8" id="F' + counter + '-end-time"></td></tr>';
+//
+//    $('#f-table').append(markup);
+//
+//});
 
 function queryModifyInfo()
 {
@@ -163,76 +161,133 @@ function queryAddCourse()
     });
 }
 
-function queryAddTime()
-{
-    for (i = 1; i < $('#m-table tr').length; i++) { 
-        var t_start = $('#M' + i + '-start-time').val();
-        var t_end = $('#M' + i + '-end-time').val();
-        if (t_start != "" && t_end != "" )
-            {
-        t_start = ConvertTimeformat(t_start);
-        t_end = ConvertTimeformat(t_end);
-                
-        $.post("server.php", { query: "add-time", day: "M", t_start: t_start, t_end: t_end }, function(data) {
-        alert(data);
-        });
-            }
-    }
+
+function queryAddTime(){
+    var days = ["M", "T", "W", "R", "F"];
     
-    for (i = 1; i < $('#t-table tr').length; i++) { 
-        var t_start = $('#T' + i + '-start-time').val();
-        var t_end = $('#T' + i + '-end-time').val();
-        t_start = ConvertTimeformat(t_start);
-        t_end = ConvertTimeformat(t_end);
-        $.post("server.php", { query: "add-time", day: "T", t_start: t_start, t_end: t_end }, function(data) {
-        alert(data);
-        });
-    }
-    
-    for (i = 1; i < $('#w-table tr').length; i++) { 
-        var t_start = $('#W' + i + '-start-time').val();
-        var t_end = $('#W' + i + '-end-time').val();
-        t_start = ConvertTimeformat(t_start);
-        t_end = ConvertTimeformat(t_end);
-        $.post("server.php", { query: "add-time", day: "W", t_start: t_start, t_end: t_end }, function(data) {
-        alert(data);
-        });
-    }
+    for(var i = 0 ; i < 5 ; i++)
+    {
         
-    for (i = 1; i < $('#r-table tr').length; i++) { 
-        var t_start = $('#R' + i + '-start-time').val();
-        var t_end = $('#R' + i + '-end-time').val();
-        t_start = ConvertTimeformat(t_start);
-        t_end = ConvertTimeformat(t_end);
-        $.post("server.php", { query: "add-time", day: "R", t_start: t_start, t_end: t_end }, function(data) {
-        alert(data);
-        });
-    }
-    
-    for (i = 1; i < $('#f-table tr').length; i++) { 
-        var t_start = $('#F' + i + '-start-time').val();
-        var t_end = $('#F' + i + '-end-time').val();
-        t_start = ConvertTimeformat(t_start);
-        t_end = ConvertTimeformat(t_end);
-        $.post("server.php", { query: "add-time", day: "F", t_start: t_start, t_end: t_end }, function(data) {
-        alert(data);
+        var t_hours = "8:00";
+        var t_start = "";
+        var t_end = "";
+        var highlighted = false;
+        var k = i + 1;
+        
+        $("#availability-table tr td:nth-child(" + k +")").each(function () {
+            
+            if ($(this).hasClass('highlighted'))
+            {
+                
+                if(highlighted == false)
+                {
+                highlighted = true;
+                t_start = t_hours;
+                }
+            }
+            else
+            {
+                if (highlighted == true)
+                {
+                    highlighted = false;
+                    t_end = t_hours;
+                    alert("Free from " + t_start + " to " + t_end + " on " + days[i]);
+                    $.post("server.php", { query: "add-time", day: days[i], t_start: t_start, t_end: t_end }, function(data) {
+                    alert(data);
+                    });
+                }   
+            }
+            t_hours = add15totime(t_hours);
         });
     }
 }
 
-function ConvertTimeformat(str) {
+function add15totime(str){
     var time = str;
     var hours = Number(time.match(/^(\d+)/)[1]);
     var minutes = Number(time.match(/:(\d+)/)[1]);
-    var AMPM = time.match(/\s(.*)$/)[1];
-    if (AMPM == "PM" && hours < 12) hours = hours + 12;
-    if (AMPM == "AM" && hours == 12) hours = hours - 12;
-    var sHours = hours.toString();
-    var sMinutes = minutes.toString();
-    if (hours < 10) sHours = "0" + sHours;
-    if (minutes < 10) sMinutes = "0" + sMinutes;
-    return(sHours + ":" + sMinutes);
+    minutes += 15;
+    var sminutes = minutes.toString();
+    if (sminutes == "60")
+        {
+            sminutes = "00";
+            hours++;     
+        }
+    
+    var shours = hours.toString();
+    return(shours + ":" + sminutes);
 }
+
+//function queryAddTime()
+//{
+//    for (i = 1; i < $('#m-table tr').length; i++) { 
+//        var t_start = $('#M' + i + '-start-time').val();
+//        var t_end = $('#M' + i + '-end-time').val();
+//        if (t_start != "" && t_end != "" )
+//            {
+//        t_start = ConvertTimeformat(t_start);
+//        t_end = ConvertTimeformat(t_end);
+//                
+//        $.post("server.php", { query: "add-time", day: "M", t_start: t_start, t_end: t_end }, function(data) {
+//        alert(data);
+//        });
+//            }
+//    }
+//    
+//    for (i = 1; i < $('#t-table tr').length; i++) { 
+//        var t_start = $('#T' + i + '-start-time').val();
+//        var t_end = $('#T' + i + '-end-time').val();
+//        t_start = ConvertTimeformat(t_start);
+//        t_end = ConvertTimeformat(t_end);
+//        $.post("server.php", { query: "add-time", day: "T", t_start: t_start, t_end: t_end }, function(data) {
+//        alert(data);
+//        });
+//    }
+//    
+//    for (i = 1; i < $('#w-table tr').length; i++) { 
+//        var t_start = $('#W' + i + '-start-time').val();
+//        var t_end = $('#W' + i + '-end-time').val();
+//        t_start = ConvertTimeformat(t_start);
+//        t_end = ConvertTimeformat(t_end);
+//        $.post("server.php", { query: "add-time", day: "W", t_start: t_start, t_end: t_end }, function(data) {
+//        alert(data);
+//        });
+//    }
+//        
+//    for (i = 1; i < $('#r-table tr').length; i++) { 
+//        var t_start = $('#R' + i + '-start-time').val();
+//        var t_end = $('#R' + i + '-end-time').val();
+//        t_start = ConvertTimeformat(t_start);
+//        t_end = ConvertTimeformat(t_end);
+//        $.post("server.php", { query: "add-time", day: "R", t_start: t_start, t_end: t_end }, function(data) {
+//        alert(data);
+//        });
+//    }
+//    
+//    for (i = 1; i < $('#f-table tr').length; i++) { 
+//        var t_start = $('#F' + i + '-start-time').val();
+//        var t_end = $('#F' + i + '-end-time').val();
+//        t_start = ConvertTimeformat(t_start);
+//        t_end = ConvertTimeformat(t_end);
+//        $.post("server.php", { query: "add-time", day: "F", t_start: t_start, t_end: t_end }, function(data) {
+//        alert(data);
+//        });
+//    }
+//}
+//
+//function ConvertTimeformat(str) {
+//    var time = str;
+//    var hours = Number(time.match(/^(\d+)/)[1]);
+//    var minutes = Number(time.match(/:(\d+)/)[1]);
+//    var AMPM = time.match(/\s(.*)$/)[1];
+//    if (AMPM == "PM" && hours < 12) hours = hours + 12;
+//    if (AMPM == "AM" && hours == 12) hours = hours - 12;
+//    var sHours = hours.toString();
+//    var sMinutes = minutes.toString();
+//    if (hours < 10) sHours = "0" + sHours;
+//    if (minutes < 10) sMinutes = "0" + sMinutes;
+//    return(sHours + ":" + sMinutes);
+//}
 
 function queryClearData()
 {
@@ -296,4 +351,37 @@ $(function() {
     $("#TAinfo").hide();
     $("#ClassCheckboxList").hide();
     $("#Admin").hide();
+    
+    // Create table dragging functionality
+          var isMouseDown = false;
+          var highlighted
+          $("table#availability-table td.selectable")
+            .mousedown(function () {
+              isMouseDown = true;
+              highlighted = $(this).hasClass('highlighted')
+
+              if ( highlighted ) {
+                $(this).removeClass('highlighted')
+              } else {
+                $(this).addClass('highlighted')
+              }
+              return false; // prevent text selection
+            })
+            .mouseover(function () {
+              if (isMouseDown) {
+                if ( highlighted ) {
+                  $(this).removeClass('highlighted')
+                } else {
+                  $(this).addClass('highlighted')
+                }
+              }
+            })
+            .bind("selectstart", function () {
+              return false; // prevent text selection in IE
+            })
+
+          $(document)
+              .mouseup(function () {
+              isMouseDown = false
+          })
 });
