342b6cdf2bc5933c22e34a696308c3b0b4e1b394
Yousef-Zoumot <yzoumot17@gmail.com>
2017-11-08 12:16:46 -0800

    fix Admin Page to refresh list qualified TAs after adding and removing an ID

--- client.js
+++ client.js
@@ -578,6 +578,7 @@ function queryListUsers()
             var markup = "";
             for(var i = 0; i < data.result.length ; i++)
                 {
+                $('#TA-list').remove(*);
                 markup = '<table style="width:100%; margin-top: 15px; margin-bottom: 20px"><tr><td style="width:33%;">' + data.result[i].studentid + ' ' + data.result[i].fname + ' ' + data.result[i].lname +'</td><td style="width:33%;">' + data.result[i].email +'</td><td style="width:33%;">' + data.result[i].phone +'</td></tr></table><hr/>';
                 $('#TA-list').append(markup);
                 }
@@ -600,7 +601,7 @@ function queryAddUser()
         alert(id);
         if(id > 0){
           alert("ID: " + id + " has been added to the list of qualified TAs");
-          location.reload();
+          queryListUsers();
         }
     });
 
@@ -613,7 +614,7 @@ function queryRemoveUser()
         alert(data);
         if(data == 0){
           alert("ID: " + id + " has been removed from the list of qualified TAs");
-          location.reload();
+          queryListUsers();
         }
     });
 }
